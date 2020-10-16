ef07ec899a585d39a39d2d64cb5e656260c07846
Nickolas Menotti <nmenotti@cox.net>
2020-10-15 19:49:22 -0700

    i dont remember what I changed, but it wasn't much

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const randomFunc = {
 
 // GENERATE EVENT LISTEN
 generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value;
+  const length = +lengthEl.values;
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
@@ -131,6 +131,8 @@ console.log(getRandomSymbol());
 // WHEN prompted for the length of the password: THEN I choose a length of at least 8 characters and no more than 128 characters
 prompt("how many characters in length would you like your password to be?     (choose between 8 and 128 characters in length)");
 
+var valueNumber = true
+
 // WHEN prompted for character types to include in the password: THEN I choose lowercase, uppercase, numeric, and/or special characters
 
 var lowercase = confirm("do you want lowercase? "); 
@@ -139,6 +141,12 @@ confirm("do you want special characters? ");
 confirm("do you want numbers? ");
 
 // WHEN I answer each prompt: THEN my input should be validated and at least one character type should be selected
+
+if (number) {
+console.log(valueNumber);
+
+}
+
 
 if (lowercase) {
   characterBase.push(valueLowercase);
